7a30d4abd645c2d8ecbc3b56de7c45e1a5904ed7
shitong xu <tonyshengxue@outlook.com>
2021-08-06 21:26:58 +0100

    xst: 21:06 08/06: finish basic torch linear regression

--- logistic_regression/basic_logit_regression.py
+++ logistic_regression/basic_logit_regression.py
@@ -16,12 +16,12 @@ training_labels = torch.from_numpy(np.c_[[0]*13 + [1]*7])
 dataset = torch.utils.data.TensorDataset(training_features, training_labels)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-# # visualize data points
-# training_labels_array = training_labels.numpy().tolist()
-# training_features_array = training_features.numpy().tolist()
-# data = pandas.DataFrame(data={'feature': training_features_array, 'label': training_labels_array})
-# data.plot(kind='scatter', x='feature', y='label')
-# plt.show()
+# visualize data points
+training_labels_array = training_labels.numpy().tolist()
+training_features_array = training_features.numpy().tolist()
+data = pandas.DataFrame(data={'feature': training_features_array, 'label': training_labels_array})
+data.plot(kind='scatter', x='feature', y='label')
+plt.show()
 
 # initialize model 
 W = torch.ones(1,1)
