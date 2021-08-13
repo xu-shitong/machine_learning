05d5f23061bda7a4b430ea3b798bfcb83bf3fc4c
shitong xu <tonyshengxue@outlook.com>
2021-08-14 00:25:56 +0100

    xst: 18:17 13/08: milestone of debugging style transfer, working example of regression on content and style loss. try get result on gpu

--- neural_network/style_transfer.py
+++ neural_network/style_transfer.py
@@ -1,3 +1,5 @@
+from mxnet.ndarray.gen_op import col2im
+from torch.autograd import backward, grad_mode
 from torch.nn import parameter
 from torchvision import transforms
 import torchvision.models as models
@@ -20,13 +22,13 @@ else:
 device = torch.device(dev)
 
 # define super parameters
-content_weight, style_weight, tv_weight = 8, 70, 1
-epoch_num = 51
+content_weight, style_weight, tv_weight = 1e-2, 1e9, 10
+epoch_num = 50
 image_shape = (150, 225)
-learning_rate = 0.004
+learning_rate = 0.01
 
 # get pretrained model
-# vgg19 = models.vgg19(pretrained=True)
+vgg19 = models.vgg19(pretrained=True).to(device)
 
 # get style and content image
 image_mean = torch.tensor([0.485, 0.456, 0.406])
@@ -42,9 +44,9 @@ transform = transforms.Compose([
 # content_image = transform(Image.open('training_data/images/rainier.jpg')).reshape((1, 3, image_shape[0], image_shape[1]))
 # content_image = content_image.to(device)
 style_image_ = nd.load('neural_network/style_img')
-style_image = torch.tensor(style_image_[0].asnumpy())
+style_image = torch.tensor(style_image_[0].asnumpy()).to(device)
 content_image_ = nd.load('neural_network/content_img')
-content_image = torch.tensor(content_image_[0].asnumpy())
+content_image = torch.tensor(content_image_[0].asnumpy()).to(device)
 
 # define output image
 # class GeneratedImage(nn.Module):
@@ -72,31 +74,69 @@ content_layers = [25]
 net = torch.load('neural_network/param.log')
 net.to(device)
 
-def extract_feature(X, style_layers, content_layers):
+def extract_feature(image, style_layers, content_layers):
   contents = []
   styles = []
+  X = image
   for i in range(0, len(net)):
     X = net[i](X)
     if i in style_layers:
-      X_copy = X.detach().clone()
-      styles.append(X_copy) 
+      # X_copy = X.detach().clone()
+      # styles.append(X_copy) 
+      styles.append(X)
     if i in content_layers:
-      X_copy = X.detach().clone()
-      contents.append(X_copy) 
+      # X_copy = X.detach().clone()
+      # contents.append(X_copy) 
+      contents.append(X)
   
   return styles, contents
+
+# def extract_feature(X, layers):
+#   features = []
+#   # X = image
+#   for i in range(29):
+#     X = vgg19.features[i](X)
+#     if i in layers:
+#       # X_copy = X.detach().clone()
+#       # styles.append(X_copy) 
+#       features.append(X)
+#       break
+  
+#   return features
 
 # define trainer and loss functions
 def gram(X):
   channel_num, n  =  X.shape[1], X.numel() // X.shape[1]
-  X = X.reshape((channel_num, n))
+  X = X.view((channel_num, n))
   return torch.mm(X, X.T) / (channel_num * n)
 
-def content_loss(content_Y_hat, content_Y):
-    return (content_Y_hat - content_Y).square().mean()
+# def gram(tensor):
+#     _, d, h, w = tensor.size()
+#     tensor = tensor.view(d, h * w)
+#     gram = torch.mm(tensor, tensor.t()) / (d * h * w)
+#     return gram 
+
+# def content_loss(content_Y_hat, content_Y):
+#     return (content_Y_hat - content_Y).square().mean()
   
-def style_loss(style_Y_hat, style_Y_gram):
-  return (gram(style_Y_hat) - style_Y_gram).square().mean()
+def content_loss(gen_feat,orig_feat):
+    #calculating the content loss of each layer by calculating the MSE between the content and generated features and adding it to content loss
+    content_l=torch.mean((gen_feat-orig_feat)**2)
+    return content_l
+
+# def style_loss(style_Y_hat, style_Y_gram):
+#   return (gram(style_Y_hat) - style_Y_gram).square().mean()
+
+def style_loss(gen,style):
+    #Calculating the gram matrix for the style and the generated image
+    batch_size,channel,height,width=gen.shape
+
+    G=torch.mm(gen.view(channel,height*width),gen.view(channel,height*width).t()) / (channel * height * width)
+    # A=torch.mm(style.view(channel,height*width),style.view(channel,height*width).t())
+
+    #Calcultating the style loss of each layer by calculating the MSE between the gram matrix of the style image and the generated image and adding it to style loss
+    style_l=torch.mean((G-style)**2)
+    return style_l
 
 def tv_loss(Y_hat):
   return 0.5 * ((Y_hat[:, :, 1:, :] - Y_hat[:, :, :-1, :]).abs().mean() +
@@ -104,50 +144,85 @@ def tv_loss(Y_hat):
 
 def loss(contents_hat, src_content, styles_Y_hat, styles_Y_gram, image):
   
-  contents_l = [content_loss(Y_hat, Y) * content_weight for Y_hat, Y in zip(
-                 contents_hat, src_content)]
+  # contents_l = [content_loss(Y_hat, Y) * content_weight for Y_hat, Y in zip(
+  #                contents_hat, src_content)]
+
+  contents_l=0
+  for Y_hat, Y in zip(contents_hat, src_content):
+    contents_l += (Y_hat - Y).square().mean()
+
+  # style_l = [style_loss(Y_hat, Y) * style_weight for Y_hat, Y in zip( styles_Y_hat, styles_Y_gram)]
   
-  style_l = [style_loss(Y_hat, Y) * style_weight for Y_hat, Y in zip( styles_Y_hat, styles_Y_gram)]
+  style_l=0
+  for Y_hat, Y in zip( styles_Y_hat, styles_Y_gram):
+    _, d, h, w = Y_hat.shape
+    style_l += (gram(Y_hat) - Y).square().mean() / (d * h * w)
   
   tv_l = tv_loss(image) * tv_weight
   
-  tot_loss = tv_l
-  for l in contents_l:
-    tot_loss = tot_loss.add(l)
-  for l in style_l:
-    tot_loss = tot_loss.add(l)
-
-  return sum(contents_l), sum(style_l), tv_l, tot_loss
+  return contents_l, style_l, tv_l
 
 trainer = optim.Adam([image], lr=learning_rate)
 # trainer = optim.SGD(image.parameters(), lr=learning_rate)
 
 # define loss from style and content image
-src_style, _ = extract_feature(X=style_image, style_layers=style_layers, content_layers=content_layers)
-_, src_content = extract_feature(X=content_image, style_layers=style_layers, content_layers=content_layers)
+src_style, _ = extract_feature(style_image, style_layers, content_layers)
+_, src_content = extract_feature(content_image, style_layers, content_layers)
 
 style_Y_gram = [gram(Y) for Y in src_style]
+# style_Y_gram = src_style
 
-show_tensor_image(un_normalize_image(image.reshape((3,150,225)), image_mean, image_std))
+# show_tensor_image(un_normalize_image(image.reshape((3,150,225)), image_mean, image_std))
 # show_tensor_image(image.reshape((3,150,225)))
 
 # training
 for i in range(epoch_num):
-  style_hat, content_hat = extract_feature(X=image, style_layers=style_layers, content_layers=content_layers)
-  c_loss, s_loss, tv_l, l = loss(content_hat, src_content, style_hat, style_Y_gram, image)
+  style_hat, content_hat = extract_feature(image, style_layers, content_layers)
+  # style_hat, content_hat = features[:-2] + features[-1:], features[-2:-1]
+  c_loss, s_loss, tv_l = loss(content_hat, src_content, style_hat, style_Y_gram, image)
+  
+  # c_loss=0
+  # for Y_hat, Y in zip(content_hat, src_content):
+  #   c_loss += (Y_hat - Y).square().mean()
+
+  # c_loss = torch.mean((content_hat[-1] - src_content[-1])**2)
+  c_loss *= content_weight
+
+  # s_loss=0
+  # for Y_hat, Y in zip(style_hat, style_Y_gram):
+  #   _, d, h, w = Y_hat.shape
+  #   s_loss += (gram(Y_hat) - Y).square().mean() / (d * h * w)
+
+  # s_loss = 0
+  # for layer in range(0, 5):
+  #     target_feature = style_hat[layer]
+  #     target_gram = gram(target_feature)
+  #     _, d, h, w = target_feature.shape
+  #     style_gram = style_Y_gram[layer]
+  #     # layer_style_loss = style_weights[layer] * torch.mean((target_gram - style_gram)**2)
+  #     layer_style_loss = torch.mean((target_gram - style_gram)**2)
+  #     s_loss += layer_style_loss / (d * h * w)
+  s_loss *= style_weight
+       
+  
+  # tv_l = tv_loss(image) * tv_weight
+  
   trainer.zero_grad()
+  tot_loss = c_loss + s_loss + tv_l 
+  # tot_loss.backward()
   if i == (epoch_num - 1):
-    l.backward()
+    tot_loss.backward()
   else:
-    l.backward(retain_graph=True)
+    tot_loss.backward(retain_graph=True)
   trainer.step()
   if (i % 10) == 0: 
-    print(f"epoch {i} loss: {c_loss}, {s_loss}, {tv_l}, total: {l}")
+    print(f"epoch {i} loss: {c_loss}, {s_loss}, {tv_l}, total: {tot_loss}")
+    # print(f"epoch {i} loss: total: {l}")
 
 # output 
 print("======== finish training =======")
 torch.save(image, 'parameter_log/style_transfer.log')
 print('data saved in parameter_log/style_transfer.log')
 
-show_tensor_image(un_normalize_image(image.reshape((3,150,225)), image_mean, image_std))
+# show_tensor_image(un_normalize_image(image.reshape((3,150,225)), image_mean, image_std))
 # show_tensor_image(image.reshape((3, 150, 225)))
---
+++ neural_network/style_transfer_example.py
@@ -0,0 +1,126 @@
+from PIL import Image
+from io import BytesIO
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+import torch.optim as optim
+import requests
+from torchvision import transforms, models
+from mxnet import nd
+import sys
+sys.path.append('../machine_learning/')
+from utils.functions import show_tensor_image, un_normalize_image
+
+
+vgg = models.vgg19(pretrained=True).features
+
+for param in vgg.parameters():
+    param.requires_grad_(False)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+vgg.to(device)
+image_mean = torch.tensor([0.485, 0.456, 0.406])
+image_std = torch.tensor([0.229, 0.224, 0.225])
+
+
+style_image_ = nd.load('neural_network/style_img')
+style = torch.tensor(style_image_[0].asnumpy())
+content_image_ = nd.load('neural_network/content_img')
+content = torch.tensor(content_image_[0].asnumpy())
+
+# def get_features(image, model, layers=None):
+    
+#     if layers is None:
+#         layers = {'0': 'conv1_1',
+#                   '5': 'conv2_1', 
+#                   '10': 'conv3_1', 
+#                   '19': 'conv4_1',
+#                   '30': 'conv5_2', #content
+#                   '28': 'conv5_1'}
+        
+#     features = {}
+#     x = image
+#     for name, layer in model._modules.items():
+#         x = layer(x)
+#         if name in layers:
+#             features[layers[name]] = x
+            
+#     return features
+
+def get_features(image, model, layers=None):
+  features = []
+  X = image
+  if layers is None:
+    layers = [0, 5,10,19,28,30] 
+
+  for i in range(31):
+    X = model[i](X)
+    if i in layers:
+      # X_copy = X.detach().clone()
+      # styles.append(X_copy) 
+      features.append(X)
+  
+  return features
+
+def gram_matrix(tensor):
+    
+    _, d, h, w = tensor.size()
+    
+    tensor = tensor.view(d, h * w)
+    
+    gram = torch.mm(tensor, tensor.t()) / (d * h * w)
+    
+    return gram 
+
+content_features = get_features(content, vgg)
+style_features = get_features(style, vgg)
+
+# style_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
+style_grams = [gram_matrix(layer) for layer in style_features[:-1]]
+
+target = content.clone().requires_grad_(True).to(device)
+
+style_weights = {'conv1_1': 1.5,
+                 'conv2_1': 0.80,
+                 'conv3_1': 0.25,
+                 'conv4_1': 0.25,
+                 'conv5_1': 0.25}
+
+content_weight = 1e-2  
+style_weight = 1e9  
+
+show = 10
+
+optimizer = optim.Adam([target], lr=0.01)
+steps = 50  
+
+show_tensor_image(un_normalize_image(target.reshape((3,150,225)), image_mean, image_std))
+
+for i in range(1, steps+1):
+    
+    target_features = get_features(target, vgg)
+    
+    content_loss = torch.mean((target_features[-1] - content_features[-1])**2)
+    
+    style_loss = 0
+    for layer in range(0, 5):
+        target_feature = target_features[layer]
+        target_gram = gram_matrix(target_feature)
+        _, d, h, w = target_feature.shape
+        style_gram = style_grams[layer]
+        # layer_style_loss = style_weights[layer] * torch.mean((target_gram - style_gram)**2)
+        layer_style_loss = torch.mean((target_gram - style_gram)**2)
+        style_loss += layer_style_loss / (d * h * w)
+        
+    total_loss = content_weight * content_loss + style_weight * style_loss
+    
+    optimizer.zero_grad()
+    total_loss.backward()
+    optimizer.step()
+    
+    if  i % show == 0:
+        print(f"epoch: {i}, c_loss: {content_weight * content_loss}, s_loss: {style_weight * style_loss}, total: {total_loss}")
+
+show_tensor_image(un_normalize_image(target.reshape((3,150,225)), image_mean, image_std))
