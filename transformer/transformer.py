18cb424dc33d65be84a56a46fa134f6850eec4e8
shitong xu <tonyshengxue@outlook.com>
2022-04-21 23:56:51 +0100

    abandon transformer, update note on parallel computing

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -1,13 +1,23 @@
 import torch
 from torch import nn
 
-class EncoderLayer(nn.Module):
-  def __init__(self, layer_num, latent_dim, input_dim, head_num) -> None:
+def attention(Q, K, V):
+  return torch.sum(torch.softmax(torch.mm(Q, K.T), dim=1) * V, dim=1)
+
+class MultiheadAttention(nn.Module):
+  def __init__(self, latent_dim, input_dim, head_num) -> None:
     super().__init__()
-    self.Ks = nn.Linear(input_dim, head_num * latent_dim)
+    self.Ks = nn.ModuleList([nn.Linear(input_dim, latent_dim // head_num) for _ in range(head_num)])
+    self.Qs = nn.ModuleList([nn.Linear(input_dim, latent_dim // head_num) for _ in range(head_num)])
+    self.Vs = nn.ModuleList([nn.Linear(input_dim, latent_dim // head_num, bias=False) for _ in range(head_num)])
+    self.K_pre = nn.Linear(input_dim, head_num * input_dim)
+    self.Q_pre = nn.Linear(input_dim, head_num * input_dim)
+    self.V_pre = nn.Linear(input_dim, head_num * input_dim)
+    self.final = nn.Linear(latent_dim, latent_dim)
 
-  def forward(self, pre_k, pre_q, pre_v):
-
+  def forward(self, x):
+    for K, Q, V in zip(self.Ks, self.Qs, self.Vs):
+      attention(K(x))
 
 
 class Encoder(nn.Module):
