b9123e6312ece7f953840e48ef1f469de5076bd3
shitong xu <tonyshengxue@outlook.com>
2022-04-30 11:41:32 +0100

    finish trial on multihead block

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -5,19 +5,20 @@ def attention(Q, K, V):
   return torch.sum(torch.softmax(torch.mm(Q, K.T), dim=1) * V, dim=1)
 
 class MultiheadAttention(nn.Module):
-  def __init__(self, latent_dim, input_dim, head_num) -> None:
+  def __init__(self, time_dim, latent_dim, d_ff, head_num) -> None:
     super().__init__()
-    self.Ks = nn.ModuleList([nn.Linear(input_dim, latent_dim // head_num) for _ in range(head_num)])
-    self.Qs = nn.ModuleList([nn.Linear(input_dim, latent_dim // head_num) for _ in range(head_num)])
-    self.Vs = nn.ModuleList([nn.Linear(input_dim, latent_dim // head_num, bias=False) for _ in range(head_num)])
-    self.K_pre = nn.Linear(input_dim, head_num * input_dim)
-    self.Q_pre = nn.Linear(input_dim, head_num * input_dim)
-    self.V_pre = nn.Linear(input_dim, head_num * input_dim)
+    self.Ks = nn.ModuleList([nn.Linear(time_dim, latent_dim // head_num, bias=False) for _ in range(head_num)])
+    self.Qs = nn.ModuleList([nn.Linear(time_dim, latent_dim // head_num, bias=False) for _ in range(head_num)])
+    self.Vs = nn.ModuleList([nn.Linear(time_dim, latent_dim // head_num, bias=False) for _ in range(head_num)])
+    self.K_pre = nn.Linear(latent_dim, latent_dim)
+    self.Q_pre = nn.Linear(latent_dim, latent_dim)
+    self.V_pre = nn.Linear(latent_dim, latent_dim)
     self.final = nn.Linear(latent_dim, latent_dim)
 
-  def forward(self, x):
+  def forward(self, x_k, x_q, x_v):
+    k, q, v = self.K_pre(x_k), self.Q_pre(x_q), self.V_pre(x_v)
     for K, Q, V in zip(self.Ks, self.Qs, self.Vs):
-      attention(K(x))
+      attention(K(k), Q(q), V(v))
 
 
 class Encoder(nn.Module):
