a01c9896c6268affdbf3eb154ee6e8a27457b2d7
xu-shitong <56657841+xu-shitong@users.noreply.github.com>
2024-07-19 16:48:07 +0100

    Create template_dataset.py

---
+++ template/template_dataset.py
@@ -0,0 +1,11 @@
+from torch.utils.data import Dataset
+        
+class ...(Dataset):
+    def __init__(self, ):
+        super(..., self).__init__()
+
+    def __len__(self):
+        return 
+    
+    def __getitem__(self, idx):
+
